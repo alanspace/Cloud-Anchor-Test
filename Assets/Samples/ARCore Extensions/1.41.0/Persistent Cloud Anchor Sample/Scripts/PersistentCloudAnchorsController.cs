24fd9f38aada71ffe8fb19fb0ac54280355afd32
alanspace <mail.alanleung@gmail.com>
2024-01-12 14:59:52 +0100

    updated

--- Assets/Samples/ARCore Extensions/1.41.0/Persistent Cloud Anchor Sample/Scripts/PersistentCloudAnchorsController.cs
+++ Assets/Samples/ARCore Extensions/1.41.0/Persistent Cloud Anchor Sample/Scripts/PersistentCloudAnchorsController.cs
@@ -51,9 +51,9 @@ namespace Google.XR.ARCoreExtensions.Samples.PersistentCloudAnchors
         public XROrigin Origin;
 #else // use ARF 4
         /// <summary>
-        /// The active XROrigin used in the example.
+        /// The active ARSessionOrigin used in the example.
         /// </summary>
-        public XROrigin SessionOrigin;
+        public ARSessionOrigin SessionOrigin;
 #endif
 
         /// <summary>
